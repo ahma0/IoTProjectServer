cb6d77aec355583401f8ccef85c714b4e7ab90bb
NadudAn <dsd932@naver.com>
2022-12-09 22:12:36 +0900

    db연동, 회원가입, 로그인

--- main.py
+++ main.py
@@ -1,42 +1,34 @@
 from flask import jsonify, Flask, request, make_response
 import pymysql
 
-# db: https://lucathree.github.io/python/day16/
+# dbw: https://lucathree.github.io/python/day16/
 
 OK = 200
 BAD_REQUEST = 400
 
 def dbcon():
     return pymysql.connect(host='localhost',
-                           user='root',
-                           password='1234',
-                           db='encore',
+                           port=3306,
+                           user='iotuser',
+                           password='iotuser123456',
+                           db='iot',
                            charset='utf8')
 
 def commitDB(sql, vals):
-    try:
-        db = dbcon()
-        cur = db.cursor()
-        cur.execute(sql, vals)
-        cur.commit()
-    except Exception as e:
-        return False
-    finally:
-        db.close()          # MySQL 연결 종료
+    db = dbcon()
+    cur = db.cursor()
+    cur.execute(sql, vals)
+    db.commit()
+    db.close()          # MySQL 연결 종료
 
     return True
 
 def selectDB(sql, vals):
-    ret = list()
-    try:
-        db = dbcon()
-        cur = db.cursor()
-        cur.execute(sql, vals)
-        ret = cur.fetchall()
-    except Exception as e:
-        return False
-    finally:
-        db.close()          # MySQL 연결 종료
+    db = dbcon()
+    cur = db.cursor()
+    cur.execute(sql, vals)
+    ret = cur.fetchone()
+    db.close()          # MySQL 연결 종료
 
     return ret
 
@@ -50,34 +42,54 @@ def signUp():
     params = request.get_json()
     carNum = params['carNo']
 
-    sql = "insert into user values(%s, %s, %s, %s)"
+    sql = "insert into member values(%s, %s, %s, %s, 0)"
     vals = (params['carNo'], params['email'], params['password'], params['phone'])
 
     flag = commitDB(sql, vals)
 
-    if(flag != True):
-        return make_response(BAD_REQUEST)
+    if(flag == False):
+        return make_response("fail")
 
-    return make_response(OK)
+    return make_response("OK")
 
 @app.route('/login', methods=['POST'])
 def login():
     # https://velog.io/@dacokim32/Flask-1Flask%EC%97%90%EC%84%9C-json%EB%8B%A4%EB%A3%A8%EA%B8%B0
     params = request.get_json()
-    sql = "SELECT id FROM user WHERE user_id = %s AND user_password = %s"
+    sql = "SELECT * FROM member WHERE member_id = %s AND member_password = %s"
     vals = (params['email'], params['password'])
 
-    flag = commitDB(sql, vals)
+    flag = selectDB(sql, vals)
 
-    if (flag != True):
+    data = {
+        "carNo": flag[0],
+        "email": flag[1],
+        "password": flag[2],
+        "phone": flag[3]
+    }
+
+    if (flag == False):
+        return make_response("FAIL")
+
+    return make_response(data)
+
+#-------------------------------------------------------------------
+
+@app.route('/mypark', methods=['GET'])
+def status():
+    params = request.get_json()
+    sql = "SELECT car_num FROM member WHERE member_id = %s"
+    flag = selectDB(sql, params['email'])
+
+    if (flag == False):
         return make_response(BAD_REQUEST)
 
-    return make_response(OK)
+    sql = "SELECT * FROM users WHERE car_num = %s"
 
 @app.route('/status', methods=['POST'])
 def status():
     params = request.get_json()
-    sql = "SELECT car_num FROM users WHERE user_id = %s"
+    sql = "SELECT car_num FROM member WHERE member_id = %s"
     flag = selectDB(sql, params['email'])
 
     if (flag == False):
